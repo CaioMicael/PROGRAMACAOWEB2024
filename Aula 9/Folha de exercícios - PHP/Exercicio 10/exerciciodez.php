948581c316f09bb622ef7d1184f0319da02cc72c
CaioMicael <142358332+CaioMicael@users.noreply.github.com>
2024-09-29 18:26:24 -0300

    commit final.

--- Aula 9/Folha de exercícios - PHP/Exercicio 10/exerciciodez.php
+++ Aula 9/Folha de exercícios - PHP/Exercicio 10/exerciciodez.php
@@ -22,12 +22,35 @@ Dado o Array abaixo, pede-se para recursivamente criar a árvore ao lado:
 --- engSoft 2
 */
 
+function imprimirArvore($array, $nivel = 0) {
+    foreach ($array as $chave => $valor) {
+        if (is_array($valor)) {
+            echo str_repeat('--', $nivel) . " " . $chave . "<br>";
+            imprimirArvore($valor, $nivel + 1);
+        } else {
+            echo str_repeat('-', $nivel + 1) . " " . $valor . "<br>";
+        }
+    }
+};
 
+// O array dado na questão
+$pastas = array(
+    "bsn" => array(
+        "3a Fase" => array(
+            "desenvWeb", 
+            "bancoDados 1", 
+            "engSoft 1"
+        ),
+        "4a Fase" => array(
+            "Intro Web", 
+            "bancoDados 2", 
+            "engSoft 2"
+        )
+    )
+        );
 
 #Programa principal
-
-
-
+imprimirArvore($pastas);
 ?>
 </body>
 </html>
